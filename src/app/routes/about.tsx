9c0e6461099d5574e5bf4be99ea7a9bdf6b43474
mutho23 <amuthohhari@gmail.com>
2026-06-02 08:58:56 +0700

    Update about.tsx

--- src/app/routes/about.tsx
+++ src/app/routes/about.tsx
@@ -48,6 +48,9 @@ export default function About() {
         <LinkItem name="Discord" href="https://discord.com/users/1134329616501309540" />
         <LinkItem name="Spotify" href="https://open.spotify.com/user/zq8df1jprwpxyiu9mkn691ai8?si=95240bc3a0ad4de8" />
         <LinkItem name="Steam" href="https://steamcommunity.com/id/moebatsu" />   
+<LinkItem name="https://popfeed.social/profile/did:plc:kxb2w63yrod2t65mlnecgrlu" />   
+
+
       </Section>
     </div>
   )
