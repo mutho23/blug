d7b425fde2597c1290cd002fab84ad12e36f7246
ghiryu <amuthohhari@gmail.com>
2026-08-01 21:48:18 +0700

    Add files via upload

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -39,7 +39,6 @@ export const loader = async () => {
 const SOCIALS = [
   {icon: '🦋', label: 'Bluesky',  href: 'https://bsky.app/profile/mutho.my.id'},
   {icon: '💬', label: 'Discord',  href: 'https://discord.gg/DNcNBQaqgM'},
-  {icon: '📷', label: 'Zeens', href: 'https://zeens.app/profile/mutho.my.id'},
   {icon: '🎵', label: 'Spotify',  href: 'https://open.spotify.com/user/zq8df1jprwpxyiu9mkn691ai8'},
   {icon: '🎮', label: 'Steam',    href: 'https://steamcommunity.com/id/moebatsu'},
   {icon: '✉️', label: 'Email',    href: 'mailto:amuthohhari@gmail.com'},
